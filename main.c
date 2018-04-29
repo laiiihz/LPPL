7de9ec8c42291e5c4e6afad761c11e96d8c4259e
laihz <laihongzhan@foxmail.com>
2018-04-29 20:32:40 +0800

    add crossplatform codes

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+char* readline(char* prompt){
+	fputs(prompt,stdout);
+	fgets(buffer,2048,stdin);
+	char* cpy=malloc(strlen(buffer)+1);
+	strcpy(cpy,buffer);
+	cpy[strlen(cpy)-1]='\0';
+	return cpy;
+}
+
+void add_history(char* unused) {
+	//TODO
+}
+#else
 #include <editline/readline.h>
+/*
+*use -ledit to link to this program
+*/
 //#include <editline/history.h>
+#endif
+
+
 
 int main(int argc,char** argv){
 	puts("Lay Plus Plus Language Version 0.0.0.1");
